d169ec5b2d57e46ec20b06f0771ce2f2891ca34d
Sean Haugh <sean@lfo.team>
2019-01-01 21:12:30 -0600

    plugin-builder: use parallel

--- overlays/plugin-builder/prefetch-plugins.rb
+++ overlays/plugin-builder/prefetch-plugins.rb
@@ -2,17 +2,14 @@
 
 require 'yaml'
 require 'json'
+require 'parallel'
 require 'progressbar'
 
 plugins = YAML.load (File.new "./plugins").read
 total_plugins = plugins["github"].length # FIXME: merge all sublists instead
-progressbar = ProgressBar.create :title => "prefetching plugins",
-    :total => total_plugins, :length => 80, :format => "%c/%C |%B| %t"
-specs = []
 
-plugins["github"].each do |plugin|
-    specs << (JSON.load `nix-prefetch-git --quiet https://github.com/#{plugin} HEAD`)
-    progressbar.increment
+specs = Parallel.map plugins["github"], progress: "Prefetching plugins" do |plugin|
+    JSON.load `nix-prefetch-git --quiet https://github.com/#{plugin} HEAD`
 end
 
 (File.new "./plugins.lock", "w").write specs.to_json

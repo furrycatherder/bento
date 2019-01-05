36d70878ab104f76b12fd744a5a48b9512cb22cb
Sean Haugh <sean@lfo.team>
2019-01-05 12:35:15 -0600

    direnv: expand hook output
    This was slowing down zsh startup time.

--- modules/programs/direnv/default.nix
+++ modules/programs/direnv/default.nix
@@ -17,7 +17,13 @@ in
 
     config = with lib; mkIf cfg.enable {
       home.files.".zshrc".text = mkIf config.programs.zsh.enable ''
-        eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
+        _direnv_hook() {
+          eval "$("${pkgs.direnv}/bin/direnv" export zsh)";
+        }
+        typeset -ag precmd_functions;
+        if [[ -z ''${precmd_functions[(r)_direnv_hook]} ]]; then
+          precmd_functions+=_direnv_hook;
+        fi
       '';
     };
   }
